cf60aca39bafd2676ff53a8c911c3cbae35432c9
iamkyrin <151950421+iamkyrin@users.noreply.github.com>
2026-01-05 17:04:19 +0200

    Enhance password hashing functionality
    Refactor password hashing loop to allow multiple inputs and handle exit condition.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import hashlib
-data = b"Hello world"
 
-print("Type in the password you want to hash: ")
-user_input = input("")
 
-encoded_input = user_input.encode('utf-8')
+try:
+    while True:
+        if 1 == 1:
+            user_input = input("Type in the password you want to hash: ")
 
-hash_obj = hashlib.sha512(encoded_input)
+            encoded_input = user_input.encode('utf-8')
 
-hex_digest = hash_obj.hexdigest()
-print(hex_digest)
+            hash_obj = hashlib.sha512(encoded_input)
+
+            hex_digest = hash_obj.hexdigest()
+            print(hex_digest)
+            user_input2 = input("Do you wish to exit? (Y/N): ").lower()
+            if user_input2 == "y":
+                break
+            elif user_input2 == "n":
+                pass
+
+except Exception as error:
+    print(error)
+    print("Error while running")
